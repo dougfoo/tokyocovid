533f7f149b653071b28d255bbd0833a4afb333dd
Doug Foo <doug.cha@gmail.com>
2020-08-22 00:12:42 +0900

    fixed lambda to run daily on own

--- cp_aws.bat
+++ cp_aws.bat
@@ -2,7 +2,5 @@
 cd ~/git/tokyocovid/material-sense/build
 aws s3 cp index.html s3://tokyocovid.foostack.org/
 aws s3 cp precache-manifest.*.js s3://tokyocovid.foostack.org/
-aws s3 cp dailyTrend.json s3://tokyocovid.foostack.org/
-aws s3 cp dailyData.json s3://tokyocovid.foostack.org/
 aws s3 cp static s3://tokyocovid.foostack.org/material-sense/static --recursive
 
