84fc6cdca89f6c27dd3ccbc2865cc03d710006e1
dougfoo <doug.cha@gmail.com>
2020-11-05 12:48:05 +0900

    lame bot fun

---
+++ slackbot.py
@@ -0,0 +1,68 @@
+import json
+from urllib import parse as urlparse
+import urllib.parse
+import base64
+
+def isPrime(i):
+    primes = {2,3}
+    if (i in primes):
+        return True
+    else:
+        if (i % 2 == 0 or i % 3 == 0):  # special since we go 3-> sqrt
+            return False
+        import math
+        sqrt = int(math.sqrt(i) // 1)
+        for s in range(3,sqrt+1,2):  # check odd vals, or all prior primes + new primes
+            if (i % s == 0):
+                return False
+        return True
+
+# not too efficient, should memoize or cache the prime{} set in isPrime()
+def nextPrime(i):
+    if isPrime(i):
+        return i
+    else:
+        return nextPrime(i+1)
+
+def factorize(i):
+    if (i == 0):
+        return []
+    elif (isPrime(i)):
+        return [i]
+    else:
+        d = 2
+        while (i % d != 0):
+            d = nextPrime(d+1)
+        rem = i // d
+        return factorize(d)+factorize(rem)
+
+
+commands = {'isPrime':isPrime,'nextPrime':nextPrime,'factorize':factorize, 'factors':factorize}
+
+def lambda_handler(event, context):
+    msg_map = dict(urlparse.parse_qsl(base64.b64decode(str(event['body'])).decode('ascii')))  # data comes b64 and also urlencoded name=value& pairs
+    command = msg_map.get('command','nac')  # will be /foo
+    params = msg_map.get('text','nat').split(" ")  # params ['isPrime','50']
+    subcommand = params[0]
+    if (len(params) < 2):
+        response = f'available subcommands: {list(commands.keys())} + 1 parameter'
+    elif (subcommand in commands.keys()):
+        response = f'{subcommand} needs an numeric param' if len(params) < 2 else f'{subcommand} = {commands[subcommand](int(float(params[1])))}'
+    else:
+        response = f'illegal sub command >{subcommand}<, commands available {list(commands.keys())}'
+
+    # logging
+    print (str(command) + ' ' + str(params) +' -> '+ response + ',original: '+ str(msg_map))
+
+    return  {
+        "response_type": "in_channel",
+        "text": command + ' ' + " ".join(params),
+        "attachments": [
+            {
+                "text":response
+            }
+        ]
+    }
+   
+    
+
